066212f5895914a91eafd0b0b70b52a155e4a45f
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-07-30 10:21:03 +0200

    Begining readme

--- levenshtein.cpp
+++ levenshtein.cpp
@@ -219,6 +219,112 @@ Automaton * dula (const unsigned int k) {
 	return dul;
 }
 
+
+void dula_direct_to_fsm (const unsigned int k, string dula_filename) {
+	string tmp_file = "__tmpFile.txt";
+	
+	Automaton * nul = nula (k);
+	Automaton & refNul = *(nul);
+	// Automaton * dul = new Automaton();
+
+	vector<DulaState *> toProceed;
+	DulaState * init = new DulaState(0);
+	init->nulaStates.insert(0);
+	dul->states[0] = init;
+	toProceed.push_back(init);
+
+	// Temporary saves;
+	map<string, DulaState *> states;
+	states[init->toString()] = init;
+
+	int idx = 1;
+	while (toProceed.size() > 0) {
+		DulaState * currentState = *(toProceed.begin());
+		DulaState & refCurState = *currentState;
+		toProceed.erase(toProceed.begin());
+
+		// For all the possible transitions add an arc and possibily create a state
+		// Warning : This is the first stupid version. Code can be massively be improved in this section.
+
+		set<string> transitions = getTransitionsFrom(refCurState, refNul);
+		set<string> nexts;
+		for (set<string>::iterator it=transitions.begin() ; it != transitions.end() ; ++it) {
+			string transition = *it;
+
+			set<int> statesHashs;
+
+			// For all the nula states in the merged dula state
+			for (set<int>::iterator hashIt=refCurState.nulaStates.begin() ; hashIt!=refCurState.nulaStates.end() ; ++hashIt) {
+				State * nState = nul->states[*hashIt];
+
+				// For all the transitions in each nula state.
+				for (int i=0 ; i<nState->accessibleStates.size() ; i++) {
+					string & targetTransition = nState->transitions[i];
+
+					if (canTransitionMatch(transition, targetTransition)) {
+						NulaState * to = (NulaState *) nState->accessibleStates[i];
+						statesHashs.insert(hashNula(to->x, to->y, k));
+					}
+				}
+			}
+
+			// Find the subsumed states...
+			set<int> toRemove;
+			for (set<int>::iterator it=statesHashs.begin() ; it!=statesHashs.end() ; ++it)
+				for (set<int>::iterator jt=next(it) ; jt!=statesHashs.end() ; ++jt) {
+					if (isDominated(*it, *jt)) {
+						toRemove.insert(*it);
+					} else if (isDominated(*jt, *it)) {
+						toRemove.insert(*jt);
+					}
+				}
+			// ... and remove them from the contracted state.
+			for (set<int>::iterator it=toRemove.begin() ; it!=toRemove.end() ; ++it)
+				statesHashs.erase(*it);
+			
+			string nextName = transitionsToString (statesHashs);
+			
+			// Merge transitions if a link already exists between current and next state.
+			if (nexts.find(nextName) != nexts.end()) {
+				DulaState * nextState = states[nextName];
+				
+				// If the nextState is already present in the accessible states vector
+				if (currentState->containsTranstionTo (nextState)) {
+					// Get the transition and contract it using the current one
+					string val = currentState->getTranstionTo (nextState);
+
+					val = contract (val, transition);
+					currentState->modifyTransitionTo(nextState, val);
+					continue;
+				}
+
+				//continue;
+			}
+			nexts.insert(nextName);
+
+			// Add the merged state if we found it for the first time.
+			if (states.find(nextName) == states.end()) {
+				DulaState * newState = new DulaState(idx);
+				for (set<int>::iterator itHash=statesHashs.begin() ; itHash!= statesHashs.end() ; ++itHash)
+					newState->nulaStates.insert(*itHash);
+				
+				// Add the state to the final automaton
+				dul->states[idx++] = newState;
+				states[nextName] = newState;
+				
+				toProceed.push_back(newState);
+			}
+
+			// Add the transition between the current and the next state.
+			State * nextState = states[nextName];
+			currentState->addTransition(nextState, transition);
+		}
+	}
+
+	delete nul;
+	return dul;
+}
+
 set<string> getTransitionsFrom(DulaState & state, Automaton & nul) {
 	std::set<string> transitions;
 
