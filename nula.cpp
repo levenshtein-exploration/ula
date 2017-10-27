8e32ad01f4d56efc6838541673be0a430f261137
yoann-dufresne <yoann.dufresne0@gmail.com>
2017-10-27 09:53:38 +0200

    add help

--- nula.cpp
+++ nula.cpp
@@ -1,23 +1,50 @@
 #include <string>
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <sstream>
 #include <stdlib.h>
+
 #include "levenshtein.h"
+
+string help () {
+	stringstream ss;
+	ss << "This software can be used to create nondeterministic Levenshtein automata." << endl;
+	ss << "Options:" << endl;
+	ss << "-dot\tOutput the nula automaton in a dot formated file (Used to create figures)" << endl;
+	ss << "-fsm\tOutput the nula automaton in fsm (finite state machine) format" << endl;
+	ss << "-h\tPrint this help" << endl;
+	ss << "-k\tThe size of the maximum Levenshtein distance wanted" << endl;
+
+	return ss.str();
+}
 
 int main (int argc, char *argv[]) {
 	int k = 1;
-	bool fsm=false, fst=false, dot=false;
+	bool fsm=false, dot=false;
+
+	if (argc == 1) {
+		cout << help();
+		exit(0);
+	}
 
 	for (int idx=1 ; idx<argc ; idx++) {
+		string arg = string(argv[idx]);
+
 		if (!strcmp(argv[idx], "-k")) {
 			idx += 1;
 			k = atoi(argv[idx]);
+
+			if (k < 0) {
+				cerr << "A Levenshtein distance can't be negative" << endl;
+				exit(1);
+			}
 		} else if (!strcmp(argv[idx], "-fsm")) {
 			fsm = true;
-		} else if (!strcmp(argv[idx], "-fst")) {
-			fst = true;
 		} else if (!strcmp(argv[idx], "-dot")) {
 			dot = true;
+		} else if (arg == "-h") {
+			cout << help();
+			exit(0);
 		} else {
 			cerr << "No option named " << argv[idx] << endl;
 			exit(1);
@@ -28,11 +55,9 @@ int main (int argc, char *argv[]) {
 	Automaton * aut = nula(k);
 	cout << aut->states.size() << " states generated" << endl;
 
-	if (dot) {
+	if (dot)
 		graphVizOutput (aut, "nula" + std::to_string(k) + ".dot");
-		if (fsm)
-			saveAutomaton (aut, "nula" + std::to_string(k) + ".fsm");
-	} else
+	if (fsm)
 		saveAutomaton (aut, "nula" + std::to_string(k) + ".fsm");
 
 	delete aut;
